049127b7da2728e02b16eb4524d8920c38732426
manaphy909 <manaphy909@gmail.com>
2026-07-25 16:03:43 -0700

    Fixed hover bubble error

--- Assets/Scripts/NPC/NPCHoverBubble.cs
+++ Assets/Scripts/NPC/NPCHoverBubble.cs
@@ -1,12 +1,18 @@
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
 public class NPCHoverBubble : MonoBehaviour
 {
     [SerializeField] private GameObject bubbleRoot;       // the world-space canvas object
     [SerializeField] private NPCInfoDisplay infoDisplay;  // lives on the same canvas
-    [SerializeField] private CCTVManager cctvController;
-    [SerializeField] private NPCManager NPCManager;
-    
+    private CCTVManager cctvController;
+    private NPCManager NPCManager;
+
+    private void Awake()
+    {
+        cctvController = GetComponent<NPCStats>().CCTVManager;
+        NPCManager = GetComponent<NPCStats>().NPCManager;
+    }
 
     void Start()
     {
